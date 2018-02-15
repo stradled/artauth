e740c8a6e12fcc99e3c940402eb3fa1d351f8be1
Martin Malloy <stradled@gmail.com>
2018-02-15 23:45:48 +0000

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <b-card title="Card Title"
+              img-src="https://lorempixel.com/600/300/food/5/"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem;"
+              class="mb-2">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
